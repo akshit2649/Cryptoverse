a5608710a2410326e30f7cb90df946f0303309a9
Akshit Thakur <akshit2649@gmail.com>
2022-06-07 14:43:10 +0530

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Layout, Typography, Space } from 'antd';
+import { Switch, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import './App.css';
 
 const App = () => {
-  return <div>Hello form the App</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default App;
