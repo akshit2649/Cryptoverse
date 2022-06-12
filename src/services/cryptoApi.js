2d590c5c2789967c2235e0fe184cf30ad9754d33
Akshit Thakur <akshit2649@gmail.com>
2022-06-12 22:29:04 +0530

    CryptoDetails

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -19,10 +19,13 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptosDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
   }),
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptosDetailsQuery } = cryptoApi;
 
 // const options = {
 //   method: 'GET',
