a6976cae49c78ea9c6553f3cd4b4d00501848fbe
Akshit Thakur <akshit2649@gmail.com>
2022-06-13 20:25:32 +0530

    Removed Exchanges

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Layout, Typography, Space } from 'antd';
 import { Switch, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Exchanges from './components/Exchanges';
 import Homepage from './components/Homepage';
 import Cryptocurrencies from './components/Cryptocurrencies';
 import CryptoDetails from './components/CryptoDetails';
@@ -23,9 +22,6 @@ const App = () => {
               <Route exact path="/">
                 <Homepage />
               </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
               <Route exact path="/cryptocurrencies">
                 <Cryptocurrencies />
               </Route>
@@ -45,7 +41,6 @@ const App = () => {
           </Typography.Title>
           <Space>
             <Link to="/">Home</Link>
-            <Link to="/exchanges">Exchanges</Link>
             <Link to="/news">News</Link>
           </Space>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import {
   HomeOutlined,
-  MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
   MenuOutlined,
@@ -53,9 +52,6 @@ const Navbar = () => {
           </Menu.Item>
           <Menu.Item icon={<FundOutlined />}>
             <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-          </Menu.Item>
-          <Menu.Item icon={<MoneyCollectOutlined />}>
-            <Link to="/exchanges">Exchanges</Link>
           </Menu.Item>
           <Menu.Item icon={<BulbOutlined />}>
             <Link to="/news">News</Link>
